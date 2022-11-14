9e97d641af86e5a3ef6d01420d6d2fa9361d13a7
HugoLorent <hugo.lorent@outlook.com>
2022-11-14 14:54:35 +0100

    rename user_id as id in user model
    Signed-off-by: HugoLorent <hugo.lorent@outlook.com>

--- src/main/java/com/example/edecision/model/User.java
+++ src/main/java/com/example/edecision/model/User.java
@@ -7,10 +7,12 @@ import javax.persistence.*;
 @Table(name = "user")
 public class User {
     @Id
-    @Column(name = "user_id")
+    @Column(name = "id")
     private String id;
+
     @Column(name = "first_name")
     private String first_name;
+
     @Column(name = "last_name")
     private String last_name;
 }
