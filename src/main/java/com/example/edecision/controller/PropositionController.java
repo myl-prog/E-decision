a13f8b8f3f9bf768f744b0854d7c86ac37443c78
Mestre Remy <mestre.remy7@gmail.com>
2023-01-05 16:42:48 +0100

    WIP : CRUD comments

--- src/main/java/com/example/edecision/controller/PropositionController.java
+++ src/main/java/com/example/edecision/controller/PropositionController.java
@@ -1,6 +1,8 @@
 package com.example.edecision.controller;
+import com.example.edecision.model.Comment;
 import com.example.edecision.model.Proposition;
 import com.example.edecision.model.User;
+import com.example.edecision.service.CommentService;
 import com.example.edecision.service.PropositionService;
 import com.example.edecision.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,9 @@ import java.util.List;
 public class PropositionController {
     @Autowired
     public PropositionService service;
+    @Autowired
+    public CommentService commentService;
+
     @GetMapping("/propositions")
     public ResponseEntity<List<Proposition>> getAll() {
         try{
@@ -38,6 +43,55 @@ public class PropositionController {
             return new ResponseEntity<>(service.create(proposition), HttpStatus.OK);
         }catch(Exception e){
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    //Get all comments
+    @GetMapping("/propositions/comments")
+    public ResponseEntity<List<Comment>> getAllComments(@PathVariable("id") Integer id) {
+        try{
+            return new ResponseEntity<>(commentService.getAll(), HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    //Get all comments of a proposition
+    @GetMapping("/propositions/{id}/comments")
+    public ResponseEntity<List<Comment>> getCommentByPropositionId(@PathVariable("id") Integer id) {
+        try{
+            return new ResponseEntity<>(commentService.getCommentsByPropositionID(id), HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    //Get comment by id of comment
+    @GetMapping("/propositions/comments/{id}")
+    public ResponseEntity<Comment> getById(@PathVariable("id") int id) {
+        try {
+            return new ResponseEntity<>(commentService.getComment(id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    //Creation of comment
+    @PostMapping("/propositions/{id}/comments")
+    public ResponseEntity<Comment> postComment(@RequestBody Comment comment, @PathVariable("id") int id) {
+        try {
+            return new ResponseEntity<>(commentService.createComment(comment, id), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    //Delete comment
+    @DeleteMapping("/propositions/comments/{id}")
+    public ResponseEntity<HttpStatus> deleteById(@PathVariable("id") int id) {
+        try {
+            commentService.deleteUser(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
---
+++ src/main/java/com/example/edecision/service/CommentService.java
@@ -0,0 +1,45 @@
+package com.example.edecision.service;
+
+import com.example.edecision.model.Comment;
+import com.example.edecision.model.Proposition;
+import com.example.edecision.model.User;
+import com.example.edecision.repository.CommentRepository;
+import com.example.edecision.repository.PropositionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CommentService {
+    @Autowired
+    public CommentRepository commentRepository;
+    public PropositionRepository propositionRepository;
+
+    public List<Comment> getAll(){
+        return commentRepository.findAll();
+    }
+
+    public Comment getComment(Integer id) {
+        return commentRepository.findById(id).get();
+    }
+    public Comment createComment(Comment comment, Integer id) {
+        comment.setId(id);
+        return commentRepository.save(comment);
+    }
+
+    public void deleteUser(Integer id) {
+        commentRepository.deleteById(id);
+    }
+    public List<Comment> getCommentsByPropositionID(Integer id) {
+        List<Comment> comments = commentRepository.findAll();
+        List<Comment> commentsByPropositionID = new ArrayList<>();
+        for (Comment comment : comments) {
+            if (comment.getProposition().getId() == id) {
+                commentsByPropositionID.add(comment);
+            }
+        }
+        return commentsByPropositionID;
+    }
+}
