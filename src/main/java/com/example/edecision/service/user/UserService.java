0749b1d420ca974710e695ca456c2669f8ac9731
HugoLorent <hugo.lorent@outlook.com>
2023-02-16 10:23:04 +0100

    add verification in updateUser method
    Signed-off-by: HugoLorent <hugo.lorent@outlook.com>

--- src/main/java/com/example/edecision/service/user/UserService.java
+++ src/main/java/com/example/edecision/service/user/UserService.java
@@ -37,17 +37,19 @@ public class UserService {
     }
 
     public User updateUser(User user, int id) {
-        User userUpdated = userRepository.findById(id).get();
-        userUpdated.setLogin(user.getLogin());
-        userUpdated.setFirst_name(user.getFirst_name());
-        userUpdated.setLast_name(user.getLast_name());
-        userUpdated.setPassword(user.getPassword());
+        if (userRepository.findById(id).isPresent()) {
+            User userUpdated = userRepository.findById(id).get();
+            userUpdated.setLogin(user.getLogin());
+            userUpdated.setFirst_name(user.getFirst_name());
+            userUpdated.setLast_name(user.getLast_name());
+            userUpdated.setPassword(user.getPassword());
 
-        if (userRepository.findByLogin(userUpdated.getLogin()).isPresent()) {
-            throw new CustomException("User with same login already exist", HttpStatus.CONFLICT);
+            if (userRepository.findByLogin(userUpdated.getLogin()).isPresent()) {
+                throw new CustomException("User with same login already exist", HttpStatus.CONFLICT);
+            }
+            return userRepository.save(userUpdated);
         }
-
-        return userRepository.save(userUpdated);
+        throw new CustomException("User not found with id : " + id, HttpStatus.NOT_FOUND);
     }
 
     public void deleteUser(int id) {
