b5c904ac43d430eda0a712de5411bb933ec076cf
Olivier Leperlier <leperlierolivier3@gmail.com>
2023-03-13 14:26:46 +0100

    complete login annotations

--- src/main/java/com/example/edecision/controller/authentication/AuthController.java
+++ src/main/java/com/example/edecision/controller/authentication/AuthController.java
@@ -4,12 +4,17 @@ import com.example.edecision.model.authentication.AuthRequest;
 import com.example.edecision.model.authentication.AuthResponse;
 import com.example.edecision.authentication.JwtTokenUtil;
 import com.example.edecision.model.user.User;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import javax.print.attribute.standard.Media;
 
 @RestController
 public class AuthController {
@@ -18,9 +23,13 @@ public class AuthController {
     @Autowired
     JwtTokenUtil jwtUtil;
 
-
-    @ApiOperation(value = "Permet à un utilisateur de se connecter avec son identifiant et son mot de passe", notes = "Retourne l'identifiant de l'utilisateur ainsi que le jwt généré qui va permettre de s'authentifier")
-    @PostMapping("/login")
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Permet à un utilisateur de se connecter avec son identifiant et son mot de passe",
+                  notes = "Retourne l'identifiant de l'utilisateur ainsi que le jwt généré qui va permettre de s'authentifier")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "L'identifiant et le mot de passe sont correct", response = AuthResponse.class),
+            @ApiResponse(code = 401, message = "Les informations renseignées ne sont pas valides")
+    })
     public ResponseEntity<?> login(@RequestBody AuthRequest request) {
         try {
             Authentication authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(request.getLogin(), request.getPassword()));
--- src/main/java/com/example/edecision/model/authentication/AuthResponse.java
+++ src/main/java/com/example/edecision/model/authentication/AuthResponse.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class AuthResponse {
+
     private String login;
     private String accessToken;
 
